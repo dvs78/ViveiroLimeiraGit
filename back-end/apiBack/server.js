777bec40ffef164abd9e75bbbcd37f2125e79a07
Daniel Veiga <dvs.veiga78@gmail.com>
2025-06-06 17:42:26 -0300

    Arrumando 2 bd em db.js

--- back-end/apiBack/server.js
+++ back-end/apiBack/server.js
@@ -27,15 +27,15 @@ import rotasPedidos from "../crud/rotas/rotasPedidos.js";
 import rotasEntregas from "../crud/rotas/rotasEntregas.js";
 
 import path from "path";
+import { fileURLToPath } from "url";
 
-const __dirname = path.resolve();
+const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// console.log("__dirname", __dirname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = express();
-export const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.use(cors());
 server.use(express.json());
